bc12747e49a5b41d1cdeea06249cf2eaf9069ee5
Carlos Dupont <dupontcam@gmail.com>
2023-05-28 11:38:58 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,11 @@
 module workshopJavafxJdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
+
 	exports gui;
 	opens gui;
+	opens model.entities to javafx.base;
+
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
