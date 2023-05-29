5cc8471c51106319b587c0a856c64a11afed0ba2
Carlos Dupont <dupontcam@gmail.com>
2023-05-29 16:49:56 -0300

    Saving a new Department

--- src/module-info.java
+++ src/module-info.java
@@ -1,13 +1,18 @@
 module workshopJavafxJdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires transitive javafx.graphics;
 	requires java.sql;
 
 	exports gui;
+	exports model.services;
+	exports model.entities;
 	opens gui;
 	opens model.entities to javafx.base;
 
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
+
+
 
