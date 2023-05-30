b1ff0cc5a8168f5806a02cec70f0137aa0e9fd7e
Carlos Dupont <dupontcam@gmail.com>
2023-05-30 12:51:30 -0300

    SellerList

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 
+
+
 public class Alerts {
 
 	public static void showAlert(String title, String header, String content, AlertType type) {
